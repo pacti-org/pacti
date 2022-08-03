b592d8f39603bcf62d3d05e150346cfa811221e0
Inigo Incer <inigo@eecs.berkeley.edu>
2022-08-03 15:38:51 -0500

    Documenting...

--- PolyhedralTerm.py
+++ PolyhedralTerm.py
@@ -17,16 +17,18 @@ class PolyhedralTerm(IoContract.Term):
     Polyhedral terms are linear inequalities over a set of variables.
 
     Usage:
-        Polyhedral terms are initialized as follows
+        Polyhedral terms are initialized as follows:
 
         .. highlight:: python
         .. code-block:: python
 
-            variables = {Var(x):2, Var(y):3}
+            variables = {Var('x'):2, Var('y'):3}
             constant = 3
             term = PolyhedralTerm(variables, constant)
 
-        Asi es
+        :code:`variables` is a dictionary whose keys are :code:`Var` instances,
+        and :code:`constant` is a number. Thus, our example represents the
+        expression :math:`2x + 3y \\le 3`.
     """
 
     # Constructor: get (i) a dictionary whose keys are variables and whose
@@ -77,8 +79,9 @@ class PolyhedralTerm(IoContract.Term):
         Variables appearing in term with a nonzero coefficient.
 
         Example:
-            For the term a*x + b*y <= c with variables x and y, this function
-            returns the set {x, y} if a and b are nonzero.
+            For the term :math:`ax + by \\le c` with variables :math:`x` and
+            :math:`y`, this function returns the set :math:`\\{x, y\\}` if
+            :math:`a` and :math:`b` are nonzero.
         """
         varlist = self.variables.keys()
         return set(varlist)
@@ -89,6 +92,10 @@ class PolyhedralTerm(IoContract.Term):
 
         Args:
             var: The variable that we are seeking in the current term.
+
+        Returns:
+            :code:`True` if the syntax of the term refers to the given variable;
+            :code:`False` otherwise.
         """
         return var in self.vars
 
@@ -106,7 +113,25 @@ class PolyhedralTerm(IoContract.Term):
 
 
     def get_polarity(self, var, polarity=True):
-        """Definition"""
+        """
+        Tells whether the polarity of a given variable in the term matches the
+        given polarity.
+
+        The polarity of a variable in a term is defined as the polarity of the
+        coefficient that multiplies it in a term, e.g., the variables :math:`x`
+        and :math:`y` in the term :math:`-2x + y \\le 3` have negative and
+        positive polarities respectively.
+
+        Args:
+            var: The variable whose polarity in the term we are seeking.
+            polarity: The polarity that we are comparing against the variable's
+            polarity.
+
+        Returns:
+            :code:`True` if the variable's polarity matches :code:`polarity` and
+            :code:`False` otherwise. If the variable's coefficient in the term
+            is zero, return :code:`True`.
+        """
         if polarity:
             return self.variables[var] >= 0
         else:
@@ -115,7 +140,35 @@ class PolyhedralTerm(IoContract.Term):
 
     def get_matching_vars(self, variable_polarity):
         """
-        Return the set of variables
+        Returns the set of variables whose polarities match the polarities
+        requested.
+
+        Example:
+
+        .. code-block:: python
+
+            x = Var('x')
+            y = Var('y')
+            z = Var('z')
+            variables = {x:-2, y:3}
+            constant  = 4
+            term = PolyhedralTerm(variables, constant)
+            polarities = {y:True}
+            term.get_matching_vars(polarities)        
+        
+        The last call returns :code:`{y, z}` because the variable y matches the
+        requested polarity in the term, and the variable z has a zero
+        coefficient. 
+
+
+        Args:
+            variable_polarity: A dictionary mapping Var instances to Boolean
+            values indicating the polarity of the given variable.
+
+        Returns:
+            If all variables in the term match the polarities specified in the
+            argument, the routine returns the matching variables.  Otherwise,
+            it returns an empty set.
         """
         variable_set = set()
         for var in variable_polarity.keys():
@@ -129,25 +182,51 @@ class PolyhedralTerm(IoContract.Term):
         return variable_set
 
 
-    def variables_math_polarity(self, variable_polarity):
-        """Definition"""
-        return len(self.get_matching_vars(variable_polarity)) > 0
-
-
     def remove_variable(self, var):
-        """Definition"""
+        """
+        Eliminates a variable from a term. This is equivalent to setting its
+        coefficient to zero.
+
+        Args:
+            var: variable to be eliminated.
+        """
         if self.contains_var(var):
             self.variables.pop(var)
 
     def multiply(self, factor):
-        """Definition"""
+        """Multiplies a term by a constant factor.
+        
+        For example, multiplying the term :math:`2x + 3y \\le 4` by the factor 2
+        yields :math:`4x + 6y \\le 8`.
+
+        Ars:
+            factor: element by which the term is multiplied.
+
+        Returns:
+            A new term which is the result of the given term multiplied by
+            :code:`factor`.
+        """
         variables = {key:factor*val for key, val in self.variables.items()}
         return PolyhedralTerm(variables, factor*self.constant)
 
-    # This routine accepts a variable to be substituted by a term and plugs in a
-    # subst term in place
+    
     def substitute_variable(self, var, subst_with_term):
-        """Definition"""
+        """
+        Substitutes a specified variable in a term with a given term.
+
+        Example:
+            In the term :math:`2x - y \\le 6`, substituting y by the term
+            :math:`x + z \\le 5` yields :math:`x - z \\le 1`. Observe that the
+            substituting term is understood as an equality.
+
+        Args:
+            var: The term variable to be substituted. subst_with_term: The term
+            used to replace var.
+
+        Returns:
+            A new term in which the variable is substituted with the given term
+            understood as an equality.
+        """
         if self.contains_var(var):
             term = subst_with_term.multiply(self.get_coefficient(var))
             logging.debug("Term is %s", term)
@@ -159,43 +238,97 @@ class PolyhedralTerm(IoContract.Term):
 
 
 
-
-
     @staticmethod
     def to_symbolic(term):
-        """Definition"""
+        """
+        Translates the variable terms of a PolyhedralTerm into a sympy
+        expression.
+
+        Example:
+            The code
+
+            .. code-block:: python
+
+            x = Var('x')
+            y = Var('y')
+            variables = {x:-2, y:3}
+            constant  = 4
+            term = PolyhedralTerm(variables, constant)
+            expression = PolyhedralTerm.to_symbolic(term)
+
+            yields for the expression :math:`-2x + 3y`.
+
+        Args:
+            term(PolyhedralTerm): The term whose coefficients and variables are
+            to be translated to sympy's data structure.
+        """
         ex = 0
         for var in term.vars:
             sv = sympy.symbols(var.name)
             ex += sv * term.get_coefficient(var)
         return ex
 
+
     @staticmethod
     def to_term(expression):
-        """Definition"""
+        """
+        Translates a sympy expression into a PolyhedralTerm.
+
+        Example:
+            The expression :math:`2x + 3y - 1` is translated into
+            :code:`PolyhedralTerm(variables={x:2, y:3}, constant=1)`.
+
+        ArgsL
+            expression: The symbolic expression to be translated.
+        """
         expression_coefficients = expression.as_coefficients_dict()
         keys = list(expression_coefficients.keys())
         variable_dict = {}
         constant = 0
         for key in keys:
             if key == 1:
-                constant = expression_coefficients[key]
+                constant = -expression_coefficients[key]
             else:
                 var = IoContract.Var(str(key))
                 variable_dict[var] = expression_coefficients[key]
         return PolyhedralTerm(variable_dict, constant)
 
     @staticmethod
-    def term_to_polytope(term, variables):
-        """Definition"""
+    def term_to_polytope(term, variable_list):
+        """
+        Transform a term into a vector according to the given order.
+
+        Example:
+            The term :math:`3x + 5y -2z \\le 7` with :code:`variable_list = [y,
+            x, w, z]` yields the tuple :code:`[5, 3, 0, -2], 7`.
+
+        Args:
+            term: The term to be transformed. variable_list: A list of variables
+            indicating the order of appearance of variable coefficients.
+
+        Returns:
+            A tuple consisting of (i) the ordered list of coefficients and (ii)
+            the term's constant.
+        """
         coeffs = []
-        for var in variables:
+        for var in variable_list:
             coeffs.append(term.get_coefficient(var))
         return coeffs, term.constant
 
     @staticmethod
     def polytope_to_term(poly, const, variables):
-        """Definition"""
+        """
+        Transform a list of coefficients and variables into a PolyhedralTerm.
+
+        Args:
+            poly: An ordered list of coefficients.
+
+            const: The term's coefficient.
+
+            variables: An ordered list of variables corresponding to the
+            coefficients.
+        """
+        assert len(poly) == len(variables)
         variable_dict = {}
         for i, var in enumerate(variables):
             variable_dict[var] = poly[i]
@@ -205,12 +338,20 @@ class PolyhedralTerm(IoContract.Term):
     @staticmethod
     def solve_for_variables(context, vars_to_elim):
         """
-        Accepts a set of terms and a set of variables that should be optimized.
+        Interpret a set of terms as equalities and solve the system of equations
+        for the given variables.
 
-        Inputs: the terms and the variables that will be optimized
+        Args:
+            context: The set of terms to be solved. Each term will be
+            interpreted as an equality.
+            
+            vars_to_elim: The set of variables whose solutions will be sought.
 
         Assumptions: the number of equations matches the number of vars_to_elim
         contained in the terms
+
+        Returns:
+            A dictionary mapping variables to their solutions.
         """
         logging.debug("GetVals: %s Vars: %s", context, vars_to_elim)
         vars_to_solve = context.vars & vars_to_elim
