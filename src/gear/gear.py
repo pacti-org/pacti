3671f6c39a077dd43532d493f14f45c600eabf38
ayush9pandey <ayush.9.pandey@gmail.com>
2022-09-10 02:39:01 -0700

    add readContract and writeContract

--- src/gear/gear.py
+++ src/gear/gear.py
@@ -50,7 +50,46 @@ def readInputFile(inputfilename,outputfilename):
     with open(outputfilename, 'w') as f:
         json.dump(data, f)
     
+def readContract(contract: dict) -> iocontract.IoContract:
+    """
+    Converts a contract written as JSON dictionary to gear.iocontract type
+    Arguments:
+        * contract (dict): A JSON dict describing the contract in the Gear syntax.
+    Returns:
+        * iocontract (gear.iocontract.IoContract): An input-output Gear contract object
+    """
+    reqs = []
+    for key in ['assumptions', 'guarantees']:
+        reqs.append([polyhedralterm.PolyhedralTerm(term['coefficients'], term['constant']) for term in contract[key]])
+    iocont = iocontract.IoContract(inputVars=getVarset(contract['InputVars']),
+                                   outputVars=getVarset(contract['OutputVars']),
+                                   assumptions=polyhedralterm.PolyhedralTermSet(set(reqs[0])),
+                                   guarantees=polyhedralterm.PolyhedralTermSet(set(reqs[1])))
+    return iocont
 
+def writeContract(contract: iocontract.IoContract, filename: str = None):
+    """
+    Converts a gear.iocontract.IoContract to a dictionary. 
+    If a filename is provided, a JSON file is written, otherwise only a dictionary is returned.
+    Arguments:
+        * contract (gear.iocontract.IoContract): Contract input of type iocontract.IoContract
+        * filename (str, optional): Name of file to write the output contract, defaults to None in which case,
+                                    no file is written.
+    Returns:
+        * contract_dict (dict): A dictionary for the given IoContract 
+    """
+    contract_dict = {}
+    contract_dict['InputVars']  = [str(var) for var in contract.inputvars]
+    contract_dict['OutputVars'] = [str(var) for var in contract.outputvars]
+    contract_dict['assumptions'] = [{'constant':str(term.constant), 'coefficients':{str(k):str(v)\
+                                   for k,v in term.variables.items()}} for term in contract.a.terms]
+    contract_dict['guarantees'] = [{'constant':str(term.constant), 'coefficients':{str(k):str(v)\
+                                  for k,v in term.variables.items()}} for term in contract.g.terms]
+    if filename:
+        with open(filename, 'w+') as f:
+            json.dump(contract_dict, f)
+    return contract_dict
+    
 def main():
     FORMAT = "[%(filename)s:%(lineno)s - %(funcName)20s() ] %(message)s"
     FORMAT1 = "[%(levelname)s:%(funcName)s()] %(message)s"
