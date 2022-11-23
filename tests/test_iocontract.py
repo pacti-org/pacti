f3faf8a619d94ee97581678bc78e1d5c8c6ea3c3
Ayush Pandey <ayush.9.pandey@gmail.com>
2022-11-22 16:33:10 -0800

    modify tests so they pass with pytest (#46)
    * modify tests so they pass with pytest
    
    * Hiding failing test
    
    * tests passing locally with these changes
    
    * run lower number of tests to speed up PRs
    
    Co-authored-by: Inigo Incer <42584466+iincer@users.noreply.github.com>

--- tests/test_iocontract.py
+++ tests/test_iocontract.py
@@ -1,12 +1,30 @@
 import gear.iocontract as iocontract
-
+from gear.terms.polyhedra.loaders import readContract
 
 def validate_iocontract(contract):
-    assert isinstance(contract, iocontract.IoContract)
+    return isinstance(contract, iocontract.IoContract)
 
-
+def create_contracts(num=1):
+    """
+    Creates `num` number of contracts and returns a list of dicts
+    """
+    contracts = []
+    for i in range(num):
+        c = {
+            "InputVars": ["i" + str(i)],
+            "OutputVars": ["o" + str(i)],
+            "assumptions": [{"coefficients": {"i" + str(i): 1}, "constant": i}],
+            "guarantees": [{"coefficients": {"o" + str(i): 1}, "constant": 1}],
+        }
+        contracts.append(c)
+    if num == 1:
+        return contracts[0]
+    else:
+        return contracts
+    
 def test_validate_iocontract():
-    exit()
+    c1, c2 = readContract(create_contracts(num=2))
+    print(type(c1))
     assert validate_iocontract(c1)
     assert validate_iocontract(c2)
 
