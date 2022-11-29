c8cc299945e59fffef28cef829ad04d51b5fdc3c
Ayush Pandey <ayush.9.pandey@gmail.com>
2022-11-29 08:35:24 +0000

    patch imports in the AND gate example and add it to tests

---
+++ tests/test_examples.py
@@ -0,0 +1,6 @@
+import pytest
+import os
+
+def test_examples():
+    result = os.popen("jupyter nbconvert --to script --execute --stdout examples/AND_logic_gate.ipynb | python3").read()
+    assert result[-1] == '\n'
