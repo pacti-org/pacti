855bc3a0594e045328d5311c0740a65b9acf085f
Sheng-Jung Yu <shengjungyu@gmail.com>
2022-11-30 16:19:50 -0800

    simple example

--- examples/dsp/gen_example1.py
+++ examples/dsp/gen_example1.py
@@ -1,4 +1,42 @@
-from tool import PortWordLength, compute_required_word_length_add, correct_under_msb_assumption
+from tool import *
+import json
+
+
+def form_contract(in_port1, in_port2, out_port):
+    ret_contract = {}
+    ret_contract["InputVars"] = [f"{in_port1.name}_a", f"{in_port1.name}_e",
+                                 f"{in_port2.name}_a", f"{in_port2.name}_e"]
+    ret_contract["OutputVars"] = [f"{out_port.name}_a", f"{out_port.name}_e"]
+    a_bound = get_assumption_bound(in_port_1=in_port1, in_port_2=in_port2, out_port=out_port)
+    ret_contract["assumptions"] =  [{"coefficients":{f"{in_port1.name}_a":1, f"{in_port2.name}_a":1},
+                                   "constant":a_bound}]
+    error_bound = get_guarantee_bound_add(in_port_1=in_port1, in_port_2=in_port2, out_port=out_port)
+    actual_val_bound = get_actual_possible_value(in_port=out_port)
+    ret_contract["guarantees"] =  [{"coefficients":{f"{in_port1.name}_e":-1, f"{in_port2.name}_e":-1, f"{out_port.name}_e": 1},
+                                  "constant":error_bound},
+                                  {"coefficients":{f"{out_port.name}_a": 1}, "constant":actual_val_bound},
+                                  ]
+    return ret_contract
+    
+def write_gear_input(contracts: list, json_path: str):
+    input_json = {}
+    for i, contract in enumerate(contracts):
+        input_json[f"contract{i+1}"] = contract
+    input_json["operation"] = "composition"
+    with open(json_path, "w") as file:
+        json.dump(input_json, file, indent=4, sort_keys=True)
+
 
 if __name__ == "__main__":
+    contracts = []
+    p1 = PortWordLength(n=5, p=2, name = "p1")
+    p2 = PortWordLength(n=5, p=3, name = "p2")
+    p3 = PortWordLength(n=5, p=3, name = "p3")
+    c1 = form_contract(in_port1=p1, in_port2=p2, out_port=p3)
+    contracts.append(c1)
+    p4 = PortWordLength(n=7, p=3, name = "p4")
+    p5 = PortWordLength(n=6, p=3, name = "p5")
+    c2 = form_contract(in_port1=p3, in_port2=p4, out_port=p5)
+    contracts.append(c2)
+    write_gear_input(contracts, "./examples/dsp/example1.json")
     pass #TODO
