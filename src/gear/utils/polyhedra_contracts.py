5ccc8137e7b5d41b5a0301b8fd081c4748a9caca
pier <piergiuseppe.mallozzi@gmail.com>
2023-01-02 17:07:51 -0800

    added topology casestudy, parser from strings and txt

---
+++ src/gear/utils/polyhedra_contracts.py
@@ -0,0 +1,17 @@
+from gear.iocontract import Var, IoContract
+from gear.terms.polyhedra import PolyhedralTerm, PolyhedralTermList
+from gear.utils.string_contract import StrContract
+
+
+def create_polyhedrea_contarct(string_contract: StrContract):
+    assumptions: list[PolyhedralTerm] = list(map(lambda x: PolyhedralTerm.from_string(x), string_contract.assumptions))
+    guarantees: list[PolyhedralTerm] = list(map(lambda x: PolyhedralTerm.from_string(x), string_contract.guarantees))
+    inputs: list[Var] = [Var(x) for x in string_contract.inputs]
+    outputs: list[Var] = [Var(x) for x in string_contract.outputs]
+
+    io_contract = IoContract(assumptions=PolyhedralTermList(assumptions),
+                             guarantees=PolyhedralTermList(guarantees),
+                             inputVars=inputs,
+                             outputVars=outputs)
+
+    print(io_contract)
