03d824c64402a1cf881b410308806deb25daf6bf
Ayush Pandey <ayush.9.pandey@gmail.com>
2023-01-11 08:19:05 +0000

    add visualization for contracts

--- src/gear/terms/polyhedra/__init__.py
+++ src/gear/terms/polyhedra/__init__.py
@@ -1,3 +1,2 @@
-from .loaders import readContract, writeContract, string_to_polyhedra_contract
 from .polyhedra import PolyhedralTerm, PolyhedralTermList
 from .loaders import read_contract, write_contract
