ed097beec287026d5dd0d0c999c4ae28da6fbb67
Apurva <abadithe@caltech.edu>
2023-01-22 00:02:12 -0800

    completed_case_study

--- case_studies/evaluating_perception/case_study.py
+++ case_studies/evaluating_perception/case_study.py
@@ -166,8 +166,10 @@ def new_initialize_ped(vmax, Ncar, true_env_type):
     bad_states |= bst_cw
     bad_states |= bst_prior
     phi = "G("+good+" | !("+bad_cw+"|" + bad_prior+"))" # eventually good
-
-    formula = "P=?[" + phi +"]" # Don't reach bad states and eventually reach good states
+    if true_env_type == "ped":
+        formula = "P=?[" + phi +"]" # Don't reach bad states and eventually reach good states
+    else:
+        formula = "P=?[G !(" + good +")]"
     print(formula)
     return xped, bad_states, good_state, formula
 # Construct backup controller:
@@ -195,7 +197,7 @@ def construct_backup_controller(Ncar, Vlow, Vhigh):
 
 # Creating the states of the markov chain for the system:
 # Returns product states S and (pos,vel) to state dictionary
-def system_states_example_ped(Ncar, Vlow, Vhigh):
+def system_states(Ncar, Vlow, Vhigh):
     nS = Ncar*(Vhigh-Vlow+1)
     state = lambda x,v: (Vhigh-Vlow+1)*(x-1) + v
     state_to_S = dict()
@@ -215,8 +217,12 @@ def get_states_and_controllers(C, M, start_state, good_state, bad_states, K, K_b
     state_info["start"] = start_state
     state_info["bad"] = bad_states
     state_info["good"] = good_state
-    for st in list(good_state):
-        formula2 = 'P=?[F(\"'+st+'\")]'
+    if true_env_type == "ped":
+        for st in list(good_state):
+            formula2 = 'P=?[F(\"'+st+'\")]'
+    else:
+        for st in list(good_state):
+            formula2 = 'P=?[G !(\"'+st+'\")]'
     MC = call_MC(K, K_backup, C, true_env, true_env_type, state_info, M)
     result2 = MC.prob_TL(formula2)
     # MC = call_MC(K, K_backup, C, true_env, true_env_type, state_info, M)
@@ -231,12 +237,10 @@ def state_f(x,v):
 # Generate probability points from Confusion Matrix:
 def gen_points(Ncar, Vlow, Vhigh, xped, bad_states, good_state, formula, vmax):
     points_ped = [] # Satisfaction probabilities for true env as ped
-    points_obj = [] # Satisfaction probabilities for true env as ped
-    points_emp = [] # Satisfaction probabilities for true env as ped
 
     vcar = 1 # Initial speed at starting point
     start_state = "S"+str(state_f(1,vcar))
-    S, state_to_S, K_backup = system_states_example_ped(Ncar, Vlow, Vhigh)
+    S, state_to_S, K_backup = system_states(Ncar, Vlow, Vhigh)
     O = {"ped", "obj", "empty"}
     tp_range = np.linspace(0.6, 0.999, num=100)
     Nrand = 15 # Fifteen random instantiations of confusion matrix for each true positive rate
@@ -255,22 +259,53 @@ def gen_points(Ncar, Vlow, Vhigh, xped, bad_states, good_state, formula, vmax):
     print("Found probabilities for pedestrian env!")
     return points_ped, tp_range_vals
 
-def gen_points_other_env():
+# Generate probability points from Confusion Matrix:
+def gen_points_obj(Ncar, Vlow, Vhigh, xped, bad_states, good_state, formula, vmax):
+    points_obj = [] # Satisfaction probabilities for true env as ped
+    vcar = 1 # Initial speed at starting point
+    start_state = "S"+str(state_f(1,vcar))
+    S, state_to_S, K_backup = system_states(Ncar, Vlow, Vhigh)
+    O = {"ped", "obj", "empty"}
+    tp_range = np.linspace(0.3, 0.999, num=100)
+    Nrand = 15 # Fifteen random instantiations of confusion matrix for each true positive rate
+    tp_range_vals = []
     for tp_obj in tp_range:
-        tp_ped = 0.8
-        tp_emp = 0.8
-        C = construct_CM(tp_ped, tp_obj, tp_emp)
-        prob = get_states_and_controllers(C, M, start_state, good_state, bad_states, K, K_backup, true_env_type="obj")
-        points_obj.append(prob)
-    print("Found probabilities for obj env!")
+        for i in range(Nrand):
+            tp_ped = 0.9
+            tp_emp = 0.9
+            K = K_des.construct_controllers(Ncar, Vlow, Vhigh, xped, vcar)
+            Mobj = MarkovChain(S, O, state_to_S)
+            Cobj = construct_CM(tp_ped, tp_obj, tp_emp)
+            MC, prob = get_states_and_controllers(Cobj, Mobj, start_state, good_state, bad_states, K, K_backup, true_env_type="obj")
+            # prob = MC.prob_TL(formula)
+            points_obj.append(prob)
+            tp_range_vals.append(tp_obj)
+    print("Found probabilities for object env!")
+    return points_obj, tp_range_vals
 
+# Generate probability points from Confusion Matrix:
+def gen_points_emp(Ncar, Vlow, Vhigh, xped, bad_states, good_state, formula, vmax):
+    points_emp = [] # Satisfaction probabilities for true env as ped
+    vcar = 1 # Initial speed at starting point
+    start_state = "S"+str(state_f(1,vcar))
+    S, state_to_S, K_backup = system_states(Ncar, Vlow, Vhigh)
+    O = {"ped", "obj", "empty"}
+    tp_range = np.linspace(0.6, 0.999, num=100)
+    Nrand = 15 # Fifteen random instantiations of confusion matrix for each true positive rate
+    tp_range_vals = []
     for tp_emp in tp_range:
-        tp_obj = 0.8
-        tp_ped = 0.8
-        C = construct_CM(tp_ped, tp_obj, tp_emp)
-        prob = get_states_and_controllers(C, M, start_state, good_state, bad_states, K, K_backup, true_env_type="empty")
-        points_emp.append(prob)
-    return points_obj, points_emp
+        for i in range(Nrand):
+            tp_obj = 0.9
+            tp_ped = 0.9
+            K = K_des.construct_controllers(Ncar, Vlow, Vhigh, xped, vcar)
+            Memp = MarkovChain(S, O, state_to_S)
+            Cemp = construct_CM(tp_ped, tp_obj, tp_emp)
+            MC, prob = get_states_and_controllers(Cemp, Memp, start_state, good_state, bad_states, K, K_backup, true_env_type="empty")
+            # prob = MC.prob_TL(formula)
+            points_emp.append(prob)
+            tp_range_vals.append(tp_emp)
+    print("Found probabilities for empty env!")
+    return points_emp, tp_range_vals
 
 # Plot probability points:
 def plot_probabilities(points, tpped_vals, true_env, vmax, rand =True):
@@ -282,12 +317,12 @@ def plot_probabilities(points, tpped_vals, true_env, vmax, rand =True):
     lb = min(tpped_vals)
     ub = max(tpped_vals)
     if rand:
-        plt.savefig("saved_plots/points_ped_lb"+str(lb)+"_ub"+str(ub)+"_vmax"+str(vmax)+"rand.png")
+        plt.savefig("saved_plots/points_"+true_env+"_lb"+str(lb)+"_ub"+str(ub)+"_vmax"+str(vmax)+"_N"+str(Ncar)+"rand.png")
     else:
-        plt.savefig("saved_plots/points_ped_lb"+str(lb)+"_ub"+str(ub)+"_vmax"+str(vmax)+".png")
+        plt.savefig("saved_plots/points_"+true_env+"_lb"+str(lb)+"_ub"+str(ub)+"_vmax"+str(vmax)+"_N"+str(Ncar)+".png")
 
 # Plot probability points:
-def plot_probabilities_bounds(points, tpped_vals, true_env, vmax, ubounds, lbounds, rand =True):
+def plot_probabilities_bounds(points, tpped_vals, true_env, vmax, Ncar, ubounds, lbounds, rand =True):
     fig, ax = plt.subplots()
     ax.set_title(true_env)
     plt.rcParams['text.usetex'] = True
@@ -295,25 +330,25 @@ def plot_probabilities_bounds(points, tpped_vals, true_env, vmax, ubounds, lboun
     plt.plot(tpped_vals, points, 'b*')
     ub_m, ub_c = ubounds
     lb_m, lb_c = lbounds
-    y_ub = ub_m*tpped_vals + ub_c
-    y_lb = lb_m*tpped_vals + lb_c
+    y_ub = ub_m*np.array(tpped_vals) + ub_c
+    y_lb = lb_m*np.array(tpped_vals) + lb_c
     plt.plot(tpped_vals, y_ub, 'r')
-    plt.plot(tpped_vals, y_lb, 'r')
+    plt.plot(tpped_vals, y_lb, 'k')
 
     lb = min(tpped_vals)
     ub = max(tpped_vals)
     if rand:
-        plt.savefig("saved_plots/bounded_points_ped_lb"+str(lb)+"_ub"+str(ub)+"_vmax"+str(vmax)+"rand.png")
+        plt.savefig("saved_plots/bounded_points_"+true_env+"_lb"+str(lb)+"_ub"+str(ub)+"_vmax"+str(vmax)+"_N"+str(Ncar)+"rand.png")
     else:
-        plt.savefig("saved_plots/bounded_points_ped_lb"+str(lb)+"_ub"+str(ub)+"_vmax"+str(vmax)+".png")
+        plt.savefig("saved_plots/bounded_points_"+true_env+"_lb"+str(lb)+"_ub"+str(ub)+"_vmax"+str(vmax)+"_N"+str(Ncar)+".png")
 
-def save_result(points_ped, tpped_vals, true_env, vmax, rand=True):
+def save_result(points_ped, tpped_vals, true_env, vmax, Ncar, rand=True):
     lb = min(tpped_vals)
     ub = max(tpped_vals)
     if rand:
-        fn = "results/points_ped_lb"+str(lb)+"_ub"+str(ub)+"_vmax"+str(vmax)+"rand.pkl"
+        fn = "results/points_"+true_env+"_lb"+str(lb)+"_ub"+str(ub)+"_vmax"+str(vmax)+"_N"+str(Ncar)+"rand.pkl"
     else:
-        fn = "results/points_ped_lb"+str(lb)+"_ub"+str(ub)+"_vmax"+str(vmax)+".pkl"
+        fn = "results/points_"+true_env+"_lb"+str(lb)+"_ub"+str(ub)+"_vmax"+str(vmax)+"_N"+str(Ncar)+".pkl"
     with open(fn, "wb") as f:
         pkl.dump([points_ped, tpped_vals], f)
     f.close()
@@ -325,24 +360,24 @@ def load_result(fn):
     f.close()
     return points_ped, tpped_vals
 
-def find_min_max_probs(result_dict):
+def find_min_max_probs(result_tuple):
     max_probs = dict()
     min_probs = dict()
-    for tpped, prob in result_dict.items():
+    for r in result_tuple:
+        tpped, prob = r
         if tpped in max_probs.keys():
             if max_probs[tpped] < prob:
                 max_probs[tpped] = prob
-            else:
-                max_probs[tpped] = prob
+        else:
+            max_probs[tpped] = prob
         if tpped in min_probs.keys():
             if min_probs[tpped] > prob:
                 min_probs[tpped] = prob
-            else:
-                min_probs[tpped] = prob
+        else:
+            min_probs[tpped] = prob
     return max_probs, min_probs
 
-
-def bound_probs(max_probs, min_probs):
+def bound_probs_obj(max_probs, min_probs):
     # Picking the two largest max_probs:
     sorted_max = sorted(max_probs.items(), key=lambda kv: kv[1])
     sorted_max = list(reversed(sorted_max))
@@ -351,11 +386,21 @@ def bound_probs(max_probs, min_probs):
     sorted_min = sorted(min_probs.items(), key=lambda kv: kv[1])
     sorted_min = collections.OrderedDict(sorted_min)
 
-    # Getting first two max points and min points
-    tp_max1, pmax1 = list(sorted_max.items())[0]
-    tp_max2, pmax2 = list(sorted_max.items())[1]
-    tp_min1, pmin1 = list(sorted_min.items())[0]
-    tp_min2, pmin2 = list(sorted_min.items())[1]
+    # Getting the eyeballed lines
+    tp_max1, pmax1 = list(sorted_max.items())[-1]
+    tp_max2, pmax2 = list(sorted_max.items())[0]
+
+    first_min_set = False
+    for tp, prob in min_probs.items():
+        if tp > 0.8 and not first_min_set:
+            tp_min1 = tp
+            pmin1 = prob
+            first_min_set = True
+            continue
+        if first_min_set:
+            tp_min2 = tp
+            pmin2 = prob
+            break
 
     # Finding line:
     ub_m = (pmax1 - pmax2)/(tp_max1 - tp_max2)
@@ -363,34 +408,125 @@ def bound_probs(max_probs, min_probs):
     lb_m = (pmin1 - pmin2)/(tp_min1 - tp_min2)
     lb_c = pmin2 - lb_m*tp_min2
     # Picking the two smallest min_probs:
+    try:
+        assert ub_m + ub_c <= 1 + 1e-4
+    except:
+        pdb.set_trace()
     ub = [ub_m, ub_c]
     lb = [lb_m, lb_c]
     return ub, lb
 
-def derive_prob_bounds(fn):
+def bound_probs_emp(max_probs, min_probs):
+    # Picking the two largest max_probs:
+    sorted_max = sorted(max_probs.items(), key=lambda kv: kv[1])
+    sorted_max = list(reversed(sorted_max))
+    sorted_max = collections.OrderedDict(sorted_max)
+
+    sorted_min = sorted(min_probs.items(), key=lambda kv: kv[1])
+    sorted_min = collections.OrderedDict(sorted_min)
+
+    # Getting the eyeballed lines
+    for tp, prob in max_probs.items():
+        if tp >= 0.9:
+            tp_max1 = tp
+            pmax1 = prob
+            break
+    tp_max2 = 1
+    pmax2 = 1
+    tp_min1, pmin1 = list(sorted_min.items())[0]
+    tp_min2, pmin2 = list(sorted_min.items())[-1]
+    # Finding line:
+    ub_m = (pmax1 - pmax2)/(tp_max1 - tp_max2)
+    ub_c = pmax2 - ub_m*tp_max2
+    lb_m = (pmin1 - pmin2)/(tp_min1 - tp_min2)
+    lb_c = pmin2 - lb_m*tp_min2
+    # Picking the two smallest min_probs:
+
+    try:
+        assert ub_m + ub_c <= 1 + 1e-4
+    except:
+        pdb.set_trace()
+    ub = [ub_m, ub_c]
+    lb = [lb_m, lb_c]
+    return ub, lb
+
+def bound_probs_ped(max_probs, min_probs):
+    # Picking the two largest max_probs:
+    sorted_max = sorted(max_probs.items(), key=lambda kv: kv[1])
+    sorted_max = list(reversed(sorted_max))
+    sorted_max = collections.OrderedDict(sorted_max)
+
+    sorted_min = sorted(min_probs.items(), key=lambda kv: kv[1])
+    sorted_min = collections.OrderedDict(sorted_min)
+
+    # Getting the eyeballed lines
+    for tp, prob in max_probs.items():
+        if tp > 0.8:
+            tp_max1 = tp
+            pmax1 = prob
+            break
+    tp_max2, pmax2 = list(sorted_max.items())[0]
+
+    for tp, prob in min_probs.items():
+        if tp > 0.8:
+            tp_min1 = tp
+            pmin1 = prob
+            break
+    tp_min2, pmin2 = list(sorted_min.items())[-1]
+
+    # Finding line:
+    ub_m = (pmax1 - pmax2)/(tp_max1 - tp_max2)
+    ub_c = pmax2 - ub_m*tp_max2
+    lb_m = (pmin1 - pmin2)/(tp_min1 - tp_min2)
+    lb_c = pmin2 - lb_m*tp_min2
+    # Picking the two smallest min_probs:
+    try:
+        assert ub_m + ub_c <= 1 + 1e-4
+        assert lb_c + ub_c <= -1e-4
+    except:
+        pdb.set_trace()
+    ub = [ub_m, ub_c]
+    lb = [lb_m, lb_c]
+    return ub, lb
+
+def derive_prob_bounds(fn, true_env):
     points_ped, tpped_vals = load_result(fn)
     lb_ped = min(tpped_vals)
     ub_ped = max(tpped_vals)
-    result_dict = {tpped_vals[i]:points_ped[i] for i in range(len(tpped_vals))}
+    result_tuple = [[tpped_vals[i],points_ped[i]] for i in range(len(tpped_vals))]
     # Finding the max and min probabilities for each true positive value:
-    max_probs, min_probs = find_min_max_probs(result_dict)
-    ub, lb = bound_probs(max_probs, min_probs)
-    return ub, lb
+    max_probs, min_probs = find_min_max_probs(result_tuple)
+    if true_env == "ped":
+        ub, lb = bound_probs_ped(max_probs, min_probs)
+    elif true_env == "obj":
+        ub, lb = bound_probs_obj(max_probs, min_probs)
+    else:
+        ub, lb = bound_probs_emp(max_probs, min_probs)
+    return ub, lb, points_ped, tpped_vals
 
 
 if __name__=='__main__':
-    MAX_V = 1
     vmax = 2
     Ncar = 6
     Vlow = 0
     Vhigh = vmax
-    xped, bad_states_ped, good_state_ped, formula_ped = new_initialize_ped(vmax, Ncar, "ped")
-    points_ped, tpped_vals = gen_points(Ncar, Vlow, Vhigh, xped, bad_states_ped, good_state_ped, formula_ped, vmax)
+    recompute = False
+    true_env = "ped"
 
-    # Save results and plot:
-    plot_probabilities(points_ped, tpped_vals, "ped", vmax)
-    fn = save_result(points_ped, tpped_vals, "ped", vmax)
-
+    if recompute:
+        xped, bad_states_ped, good_state_ped, formula_ped = new_initialize_ped(vmax, Ncar, true_env)
+        if true_env == "ped":
+            points, tp_vals = gen_points(Ncar, Vlow, Vhigh, xped, bad_states_ped, good_state_ped, formula_ped, vmax)
+        elif true_env == "obj":
+            points, tp_vals = gen_points_obj(Ncar, Vlow, Vhigh, xped, bad_states_ped, good_state_ped, formula_ped, vmax)
+        elif true_env == "empty":
+            points, tp_vals = gen_points_emp(Ncar, Vlow, Vhigh, xped, bad_states_ped, good_state_ped, formula_ped, vmax)
+        # Save results and plot:
+        plot_probabilities(points, tp_vals, true_env, vmax)
+        fn = save_result(points, tp_vals, true_env, vmax, Ncar)
     # Derive bounds and plot probability bounds:
-    ubounds, lbounds = derive_prob_bounds(fn)
-    plot_probabilities_bounds(points, tpped_vals, true_env, vmax, ubounds, lbounds, rand =True)
+    else:
+        fn = "results/points_"+true_env+"_lb0.6_ub0.999_vmax2_N6rand.pkl"
+
+    ubounds, lbounds, points, tp_vals = derive_prob_bounds(fn, true_env)
+    plot_probabilities_bounds(points, tp_vals, true_env, vmax, Ncar, ubounds, lbounds, rand =True)
