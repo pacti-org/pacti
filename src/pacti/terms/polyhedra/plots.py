9b8d142e94995a93e2ff2dc542aaae534fe6abfa
Inigo Incer <inigo@eecs.berkeley.edu>
2023-02-14 15:12:21 -0800

    basic plotting functionality is in shape

--- src/pacti/terms/polyhedra/plots.py
+++ src/pacti/terms/polyhedra/plots.py
@@ -19,20 +19,68 @@ from matplotlib.figure import Figure
 numeric = Union[int, float]
 
 def plot_assumptions(contract:IoContract, x_var:Var, y_var:Var, var_values:dict[Var,numeric], x_lims:tuple[numeric], y_lims: tuple[numeric]) -> Figure:
+    """
+    Plots the assumptions of an IoContract with polyhedral terms.
+
+    Args:
+        contract: the contract whose assumptions will be plotted.
+        x_var: variable mapped to the x-axis.
+        y_var: variable mapped to the y-axis.
+        var_values: values of other variables appearing in the assumptions.
+        x_lims: range of values in the x-axis.
+        y_lims: range of values in the y-axis.
+
+    Returns:
+        Figure element with a single "axes" object showing the feasible region for the assumptions.
+    """
     if not(x_var in contract.vars):
         raise ValueError("Variable %s is not in an input or output variable of contract." % (x_var))
     if not(y_var in contract.vars):
         raise ValueError("Variable %s is not in an input or output variable of contract." % (y_var))
-    if not isinstance(contract.a, PolyhedralTermList):
-        raise ValueError("Expecting contract with polyhedral assumptions. Assumptions type: %s" %(type(contract.a)))
     for var in var_values.keys():
         if not var in contract.vars:
             raise ValueError("Var %s from var_values is not in the interface of the contract." %(var))
-    diff = list_diff(contract.vars, list_union([x_var,y_var], list(var_values.keys())))
+    fig = _plot_constraints(contract.a,x_var, y_var, var_values, x_lims, y_lims)
+    ax = fig.axes[0]
+    ax.set_title("Assumptions")
+    return fig
+
+def plot_guarantees(contract:IoContract, x_var:Var, y_var:Var, var_values:dict[Var,numeric], x_lims:tuple[numeric], y_lims: tuple[numeric]) -> Figure:
+    """
+    Plots the guarantees and assumptions of an IoContract with polyhedral terms.
+
+    Args:
+        contract: the contract whose assumptions & guarantees will be plotted.
+        x_var: variable mapped to the x-axis.
+        y_var: variable mapped to the y-axis.
+        var_values: values of other variables appearing in the assumptions & guarantees.
+        x_lims: range of values in the x-axis.
+        y_lims: range of values in the y-axis.
+
+    Returns:
+        Figure element with a single "axes" object showing the feasible region for the assumptions & guarantees.
+    """
+    if not(x_var in contract.vars):
+        raise ValueError("Variable %s is not in an input or output variable of contract." % (x_var))
+    if not(y_var in contract.vars):
+        raise ValueError("Variable %s is not in an input or output variable of contract." % (y_var))
+    for var in var_values.keys():
+        if not var in contract.vars:
+            raise ValueError("Var %s from var_values is not in the interface of the contract." %(var))
+    fig = _plot_constraints(contract.a | contract.g, x_var, y_var, var_values, x_lims, y_lims)
+    ax = fig.axes[0]
+    ax.set_title("Guarantees")
+    return fig
+
+
+def _plot_constraints(constraints:PolyhedralTermList, x_var:Var, y_var:Var, var_values:dict[Var,numeric], x_lims:tuple[numeric], y_lims: tuple[numeric]) -> Figure:
+    if not isinstance(constraints, PolyhedralTermList):
+        raise ValueError("Expecting polyhedral constraints. Constraint type: %s" %(type(constraints)))
+    diff = list_diff(constraints.vars, list_union([x_var,y_var], list(var_values.keys())))
     if diff:
         raise ValueError("Need to set variables %s" % (diff))
     # for every term in the assumptions, replace all variables with the values provided
-    term_list = contract.a.copy()
+    term_list = constraints.copy()
     term_list = term_list | PolyhedralTermList([PolyhedralTerm({x_var:1},x_lims[1]), PolyhedralTerm({x_var:-1},-x_lims[0]), PolyhedralTerm({y_var:1},y_lims[1]), PolyhedralTerm({y_var:-1},-y_lims[0])]) 
     plot_list = []
     for term in term_list.terms:
@@ -40,7 +88,7 @@ def plot_assumptions(contract:IoContract, x_var:Var, y_var:Var, var_values:dict[
             term = term.substitute_variable(var=var, subst_with_term=PolyhedralTerm(variables={},constant=-val))
         # we may have eliminated all variables after substitution
         if not term.vars:
-            if term.contant < 0:
+            if term.constant < 0:
                 print("Assumptions are unfeasible")
                 return
             else:
@@ -72,9 +120,8 @@ def plot_assumptions(contract:IoContract, x_var:Var, y_var:Var, var_values:dict[
     # Now that we have all vertices, we can plot the polygon
     x, y = zip(*hs.intersections)
     # sort the points in polar coordinates
-    centroid=(sum(x)/len(x),sum(y)/len(y))
-    # sort by angle
-    points = sorted(zip(x,y), key= lambda p: atan2(p[1]-centroid[1],p[0]-centroid[0]))
+    center=(sum(x)/len(x),sum(y)/len(y))
+    points = sorted(zip(x,y), key= lambda p: atan2(p[1]-center[1],p[0]-center[0]))
     x,y = zip(*points)
 
     # generate figure
@@ -83,7 +130,7 @@ def plot_assumptions(contract:IoContract, x_var:Var, y_var:Var, var_values:dict[
     ax.set_xlim(x_lims)
     ax.set_ylim(y_lims)
     ax.set_xlabel(x_var.name)
-    ax.set_ylabel(x_var.name)
+    ax.set_ylabel(y_var.name)
 
     poly = Polygon(np.column_stack([x, y]), animated=False,closed=True)
     ax.add_patch(poly)
@@ -116,4 +163,5 @@ if __name__ == "__main__":
     }
     c1 = read_contract(contract1)
     fig = plot_assumptions(contract=c1,x_var=Var("u_1"),y_var=Var("u_2"),var_values={Var("x_1"):0},x_lims=(-2,2),y_lims=(-2,2))
+    fig = plot_guarantees(contract=c1,x_var=Var("u_1"),y_var=Var("x_1"),var_values={Var("u_2"):1},x_lims=(-2,2),y_lims=(-2,2))
     plt.show()
