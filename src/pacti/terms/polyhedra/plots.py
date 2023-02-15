e421e39d3276f54e367833d9fde4d010c3baa628
Inigo Incer <inigo@eecs.berkeley.edu>
2023-02-14 21:23:00 -0800

    giving errors when setting values to axes variables

--- src/pacti/terms/polyhedra/plots.py
+++ src/pacti/terms/polyhedra/plots.py
@@ -76,6 +76,10 @@ def plot_guarantees(contract:IoContract, x_var:Var, y_var:Var, var_values:dict[V
 def _plot_constraints(constraints:PolyhedralTermList, x_var:Var, y_var:Var, var_values:dict[Var,numeric], x_lims:tuple[numeric], y_lims: tuple[numeric]) -> Figure:
     if not isinstance(constraints, PolyhedralTermList):
         raise ValueError("Expecting polyhedral constraints. Constraint type: %s" %(type(constraints)))
+    if x_var in var_values.keys():
+        raise ValueError("x variable %s can't be assigned a value" % (x_var))
+    if y_var in var_values.keys():
+        raise ValueError("y variable %s can't be assigned a value" % (y_var))
     diff = list_diff(constraints.vars, list_union([x_var,y_var], list(var_values.keys())))
     if diff:
         raise ValueError("Need to set variables %s" % (diff))
@@ -166,34 +170,57 @@ if __name__ == "__main__":
     #fig = plot_guarantees(contract=c1,x_var=Var("u_1"),y_var=Var("x_1"),var_values={Var("u_2"):1},x_lims=(-2,2),y_lims=(-2,2))
     #plt.show()
 
-    contract2 = {
-        "InputVars":[
-            "t0", "v0"
-        ],
-        "OutputVars":[
-            "t1", "v1"
-        ],
-        "assumptions":
-        [
-            {"coefficients":{"v0":1}, "constant":20000}
-        ],
-        "guarantees":
-        [
-            {"coefficients":{"t1":-1}, "constant":-90},
-            {"coefficients":{"v1":-1}, "constant":-1600.00}
-        ]
-    }
-    c2 = PolyhedralContract.from_dict(contract2)
-    print(c2.vars)
-    fig = plot_guarantees(
-        contract=c2,
-        x_var=Var("t0"),
-        y_var=Var("v1"),
-        var_values={
-            Var("t1"):91,
-            Var("v0"):20000.0
-        },
-        x_lims=(-10,100),
-        y_lims=(1000, 21000)
-        )
+    # contract2 = {
+    #     "InputVars":[
+    #         "t0", "v0"
+    #     ],
+    #     "OutputVars":[
+    #         "t1", "v1"
+    #     ],
+    #     "assumptions":
+    #     [
+    #         {"coefficients":{"v0":1}, "constant":20000}
+    #     ],
+    #     "guarantees":
+    #     [
+    #         {"coefficients":{"t1":-1}, "constant":-90},
+    #         {"coefficients":{"v1":-1}, "constant":-1600.00}
+    #     ]
+    # }
+    # c2 = PolyhedralContract.from_dict(contract2)
+    # print(c2.vars)
+    # fig = plot_guarantees(
+    #     contract=c2,
+    #     x_var=Var("t0"),
+    #     y_var=Var("v1"),
+    #     var_values={
+    #         Var("t1"):91,
+    #         Var("v0"):20000.0
+    #     },
+    #     x_lims=(-10,100),
+    #     y_lims=(1000, 21000)
+    #     )
+    # plt.show()
+
+    contract3 = {"InputVars": ["t10", "soc10", "d10", "e10", "r10"], "OutputVars": ["t11", "soc11", "d11", "e11", "r11"], "assumptions": [{"constant": -6.0, "coefficients": {"soc10": -1.0}}, {"constant": -1.0, "coefficients": {"d10": -1.0}}], "guarantees": [{"constant": 2.0, "coefficients": {"t11": 1.0, "t10": -1.0}}, {"constant": -2.0, "coefficients": {"t11": -1.0, "t10": 1.0}}, {"constant": 6.0, "coefficients": {"soc10": 1.0, "soc11": -1.0}}, {"constant": 0.0, "coefficients": {"d11": 1.0}}, {"constant": 0.0, "coefficients": {"d11": -1.0}}, {"constant": 0.0, "coefficients": {"e11": 1.0, "e10": -1.0}}, {"constant": 0.0, "coefficients": {"e11": -1.0, "e10": 1.0}}, {"constant": 0.0, "coefficients": {"r11": 1.0, "r10": -1.0}}, {"constant": 0.0, "coefficients": {"r11": -1.0, "r10": 1.0}}]}
+    c3 = PolyhedralContract.from_dict(contract3)
+
+    fig = plot_guarantees(contract=c3,
+                x_var=Var("t10"),
+                y_var=Var("soc11"),
+                var_values={
+                  #Var("t10"):0,
+                  Var("t11"):2,
+                  Var("d10"):1,
+                  Var("d11"):0,
+                  Var("e10"):0,
+                  Var("e11"):0,
+                  Var("r10"):0,
+                  Var("r11"):0,
+                  Var("soc10"):6,
+                },
+                x_lims=(-2,2),
+                y_lims=(-2,2))
     plt.show()
+
+
