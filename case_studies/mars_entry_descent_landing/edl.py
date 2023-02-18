0ea978a90dac2197695fee9ccf00c8912878f90e
Nicolas Rouquette <nfr@jpl.nasa.gov>
2023-02-18 15:31:01 -0800

    working quotient

---
+++ case_studies/mars_entry_descent_landing/edl.py
@@ -0,0 +1,11 @@
+atmospheric_v_entry = 20000.0
+atmospheric_v_exit = 1600.0 
+
+atmospheric_t_entry = 0.0
+atmospheric_t_exit = 90.0
+
+skycrane_v_entry = 2.7
+skycrane_v_exit = 0
+
+skycrane_t_entry = 398.0
+skycrane_t_exit = 410.0
