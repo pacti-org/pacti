4ba5d6470bbda6506b4ba915465f1fc7224ad2c2
Inigo Incer <42584466+iincer@users.noreply.github.com>
2023-02-25 21:03:57 -0800

    203 composec1 c2 keep add an optional argument keep to preserve some of c1s outputs (#205)
    * Composition allows the user to keep variables
    
    * typing + formatting

--- src/pacti/terms/polyhedra/polyhedral_contract.py
+++ src/pacti/terms/polyhedra/polyhedral_contract.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 from functools import reduce
+from typing import Optional
 
 import pacti.terms.polyhedra.serializer as serializer
 from pacti.iocontract import IoContract, IoContractCompound, NestedTermList, Var
@@ -61,6 +62,11 @@ class PolyhedralContract(IoContract):
             assumptions=a,
             guarantees=g,
         )
+
+    def compose(self, other: PolyhedralContract, vars_to_keep: Optional[list[str]] = None):
+        if vars_to_keep is None:
+            vars_to_keep = []
+        return super().compose(other, [Var(x) for x in vars_to_keep])
 
 
 class NestedPolyhedra(NestedTermList):
