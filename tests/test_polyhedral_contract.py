ccc2f16e37dfee0b6720d69ddca68ad1b20648b7
Inigo Incer <42584466+iincer@users.noreply.github.com>
2023-03-01 13:11:53 -0800

    136 improve code quality (#214)
    * improving docs build process
    * removing CLI
    * test infrastructure for contracts

---
+++ tests/test_polyhedral_contract.py
@@ -0,0 +1,16 @@
+from pacti.terms.polyhedra import PolyhedralContract
+
+TEST_DATA_DIR = "tests/test_data/polyhedral_contracts/"
+
+
+def test_composition_1():
+    c = PolyhedralContract.from_file(TEST_DATA_DIR + "test_composition_1.json")
+    expected = c[2]
+    obtained = c[0].compose(c[1])
+    print(expected)
+    print(obtained)
+    assert expected == obtained
+
+
+if __name__ == "__main__":
+    test_composition_1()
