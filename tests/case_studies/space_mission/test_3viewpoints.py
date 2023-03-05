5bbba84e14988eafd7ec836de36b82dca56d9346
Nicolas Rouquette <nfr@jpl.nasa.gov>
2023-03-04 16:28:50 -0800

    notebook execytion test

---
+++ tests/case_studies/space_mission/test_3viewpoints.py
@@ -0,0 +1,42 @@
+import nbformat
+import os
+from nbconvert.preprocessors import ExecutePreprocessor
+
+
+def _read_notebook(filename: str):
+    with open(filename) as ff:
+        return nbformat.read(ff, nbformat.NO_CONVERT)
+
+j1 = '../../../case_studies/space_mission/scenario_power.json'
+j2 = '../../../case_studies/space_mission/scenario_science.json'
+j3 = '../../../case_studies/space_mission/scenario_navigation.json'
+
+def test_viewpoints():
+    os.remove(j1)
+    nb1 = _read_notebook('../../../case_studies/space_mission/space_mission_power.ipynb')
+
+    os.remove(j2)
+    nb2 = _read_notebook('../../../case_studies/space_mission/space_mission_science.ipynb')
+    
+    os.remove(j3)
+    nb3 = _read_notebook('../../../case_studies/space_mission/space_mission_navigation.ipynb')
+    nb4 = _read_notebook('../../../case_studies/space_mission/space_mission_3viewpoints.ipynb')
+    
+    
+    ep = ExecutePreprocessor(timeout=600, kernel_name='python3')
+
+    # Run the 1st notebook and write the scenario
+    r1 = ep.preprocess(nb1)
+    assert os.path.exists(j1)
+    
+    # Run the 2nd notebook and write the scenario
+    r2 = ep.preprocess(nb2)
+    assert os.path.exists(j2)
+
+    # Run the 3rd notebook and write the scenario
+    r3 = ep.preprocess(nb3)
+    assert os.path.exists(j3)
+
+    # This should read all 3 scenario json files and analyze them.
+    r4 = ep.preprocess(nb4)
+    print(r4)
