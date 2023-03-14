ec3b99fd4ca9d91fb29428a597a39433fde5f51a
Inigo Incer <inigo@eecs.berkeley.edu>
2023-03-13 22:56:59 -0700

    Reducing load on python code Better errors in iocontract

--- examples/issues/var_elim/issue.py
+++ examples/issues/var_elim/issue.py
@@ -2,6 +2,7 @@
 
 
 from pacti import read_contracts_from_file
+from pacti.iocontract import Var
 import logging
 
 FORMAT = "%(asctime)s:%(levelname)s:%(name)s:%(message)s"
@@ -9,4 +10,6 @@ logging.basicConfig(filename="../pacti.log", filemode="w", level=logging.DEBUG, 
 
 conts, _ = read_contracts_from_file("var_elim.json")
 
-result = conts[0].compose(conts[1])
+result = conts[0].compose(conts[1], vars_to_keep=[Var('d2_exit'), Var('c2_exit')])
+
+print(result)
