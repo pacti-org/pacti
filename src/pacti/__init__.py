b6679fe3d30edb2db3699af407eba955e63996cc
Ayush Pandey <ayush.9.pandey@gmail.com>
2023-04-13 06:49:34 -0700

    Versioning in Pacti (#311)
    * add pacti __version__
    * Add version querying feature

--- src/pacti/__init__.py
+++ src/pacti/__init__.py
@@ -1,1 +1,2 @@
 from .utils import read_contracts_from_file, write_contracts_to_file
+from .__version__ import __version__
---
+++ src/pacti/__version__.py
@@ -0,0 +1,5 @@
+"""
+Set the Pacti package version in this file
+"""
+
+__version__ = '1.0.0.alpha'
