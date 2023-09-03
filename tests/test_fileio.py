d77012758335c7840500ae23af075d0fecedc1ff
Inigo Incer <inigo@eecs.berkeley.edu>
2023-09-03 00:33:49 -0700

    Adding fileiotest

---
+++ tests/test_fileio.py
@@ -0,0 +1,29 @@
+
+from pacti.utils import read_contracts_from_file, write_contracts_to_file
+
+
+def test_basic_fileio():
+    io_poly_compound = "tests/test_data/compound_contracts/test_merging_success_multiagent_1.json"
+    io_poly = "tests/test_data/polyhedral_contracts/test_quotient_success_1.json"
+
+
+    c_list, c_names = read_contracts_from_file(io_poly)
+
+    for machine in [True, False]:
+        write_contracts_to_file(c_list, c_names, file_name="test_fileio.json", machine_representation=machine)
+        c_list_read, c_names_read = read_contracts_from_file("test_fileio.json")
+        for i in range(len(c_list)):
+            assert c_list_read[i] == c_list[i]
+        assert c_names_read == c_names
+
+    c_list, c_names = read_contracts_from_file(io_poly_compound)
+    write_contracts_to_file(c_list, c_names, file_name="test_fileio.json", machine_representation=False)
+    c_list_read, c_names_read = read_contracts_from_file("test_fileio.json")
+    for i in range(len(c_list)):
+        assert c_list_read[i] == c_list[i]
+    assert c_names_read == c_names
+
+if __name__ == "__main__":
+    test_basic_fileio()
+
+    
