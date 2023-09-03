8f25ac92b4b4b5f847ef03e911f4ea613a5a8092
Inigo Incer <inigo@eecs.berkeley.edu>
2023-09-03 09:02:56 -0700

    No magical file names

--- tests/test_fileio.py
+++ tests/test_fileio.py
@@ -1,4 +1,5 @@
 
+import tempfile
 from pacti.utils import read_contracts_from_file, write_contracts_to_file
 
 
@@ -8,17 +9,18 @@ def test_basic_fileio():
 
 
     c_list, c_names = read_contracts_from_file(io_poly)
+    tempfile_name = tempfile.NamedTemporaryFile().name
 
     for machine in [True, False]:
-        write_contracts_to_file(c_list, c_names, file_name="test_fileio.json", machine_representation=machine)
-        c_list_read, c_names_read = read_contracts_from_file("test_fileio.json")
+        write_contracts_to_file(c_list, c_names, file_name=tempfile_name, machine_representation=machine)
+        c_list_read, c_names_read = read_contracts_from_file(tempfile_name)
         for i in range(len(c_list)):
             assert c_list_read[i] == c_list[i]
         assert c_names_read == c_names
 
     c_list, c_names = read_contracts_from_file(io_poly_compound)
-    write_contracts_to_file(c_list, c_names, file_name="test_fileio.json", machine_representation=False)
-    c_list_read, c_names_read = read_contracts_from_file("test_fileio.json")
+    write_contracts_to_file(c_list, c_names, file_name=tempfile_name, machine_representation=False)
+    c_list_read, c_names_read = read_contracts_from_file(tempfile_name)
     for i in range(len(c_list)):
         assert c_list_read[i] == c_list[i]
     assert c_names_read == c_names
