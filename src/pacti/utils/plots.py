a3eb32939a1928f07dd3fa3449c52aaa719e764b
Inigo Incer <inigo@eecs.berkeley.edu>
2023-09-02 19:32:31 -0700

    Added test for plotting

--- src/pacti/utils/plots.py
+++ src/pacti/utils/plots.py
@@ -99,8 +99,8 @@ def plot_guarantees(
     y_lims: Tuple[numeric, numeric],
     new_x_var: Optional[str] = None,
     new_y_var: Optional[str] = None,
-    x_transform: Optional[Callable[[numeric], numeric]] = None,
-    y_transform: Optional[Callable[[numeric], numeric]] = None,
+    x_transform: Optional[Callable[[numeric, numeric], numeric]] = None,
+    y_transform: Optional[Callable[[numeric, numeric], numeric]] = None,
     number_of_points: Optional[int] = 30,
     show: Optional[bool] = True,
 ) -> MplFigure:
@@ -343,8 +343,7 @@ def get_path(
         A tuple of x tuples and y tuples corresponding to the transformed path.
     """
     xx = np.linspace(x0, x1, number_of_points)
-    m = (y1 - y0) / (x1 - x0)
-    yy = m * (xx - x0) + y0
+    yy = np.linspace(y0, y1, number_of_points)
     x_tranform_vec = np.vectorize(x_transform)
     y_tranform_vec = np.vectorize(y_transform)
     return x_tranform_vec(xx, yy).tolist(), y_tranform_vec(xx, yy).tolist()
@@ -388,85 +387,3 @@ def _plot_transformed_constraints(
         plt.close(fig)
 
     return fig
-
-
-if __name__ == "__main__":
-    contract1 = {
-        "input_vars": ["u_1", "u_2"],
-        "output_vars": ["x_1"],
-        "assumptions": [
-            {"coefficients": {"u_1": -1, "u_2": 0}, "constant": 0},
-            {"coefficients": {"u_1": 1, "u_2": 0}, "constant": 1},
-            {"coefficients": {"u_1": 0, "u_2": -1}, "constant": 0},
-            {"coefficients": {"u_1": 0, "u_2": 1}, "constant": 1},
-        ],
-        "guarantees": [{"coefficients": {"x_1": -1}, "constant": -1.5}],
-    }
-    c1 = PolyhedralIoContract.from_dict(contract1)
-    fig = plot_assumptions(
-        contract=c1, x_var=Var("u_1"), y_var=Var("u_2"), var_values={Var("x_1"): 0}, x_lims=(-2, 2), y_lims=(-2, 2)
-    )
-    fig = plot_guarantees(
-        contract=c1, x_var=Var("u_1"), y_var=Var("x_1"), var_values={Var("u_2"): 1}, x_lims=(-2, 2), y_lims=(-2, 2)
-    )
-
-    contract2 = {
-        "input_vars": ["t0", "v0"],
-        "output_vars": ["t1", "v1"],
-        "assumptions": [{"coefficients": {"v0": 1}, "constant": 20000}],
-        "guarantees": [
-            {"coefficients": {"t1": -1}, "constant": -90},
-            {"coefficients": {"v1": -1}, "constant": -1600},
-        ],
-    }
-    c2 = PolyhedralIoContract.from_dict(contract2)
-    fig = plot_guarantees(
-        contract=c2,
-        x_var=Var("t0"),
-        y_var=Var("v1"),
-        var_values={Var("t1"): 91, Var("v0"): 20000},
-        x_lims=(-10, 100),
-        y_lims=(1000, 21000),
-    )
-
-    contract3 = {
-        "input_vars": ["t10", "soc10", "d10", "e10", "r10"],
-        "output_vars": ["t11", "soc11", "d11", "e11", "r11"],
-        "assumptions": [
-            {"constant": -6.0, "coefficients": {"soc10": -1.0}},
-            {"constant": -1.0, "coefficients": {"d10": -1.0}},
-        ],
-        "guarantees": [
-            {"constant": 2.0, "coefficients": {"t11": 1.0, "t10": -1.0}},
-            {"constant": -2.0, "coefficients": {"t11": -1.0, "t10": 1.0}},
-            {"constant": 6.0, "coefficients": {"soc10": 1.0, "soc11": -1.0}},
-            {"constant": float(0), "coefficients": {"d11": 1.0}},
-            {"constant": float(0), "coefficients": {"d11": -1.0}},
-            {"constant": float(0), "coefficients": {"e11": 1.0, "e10": -1.0}},
-            {"constant": float(0), "coefficients": {"e11": -1.0, "e10": 1.0}},
-            {"constant": float(0), "coefficients": {"r11": 1.0, "r10": -1.0}},
-            {"constant": float(0), "coefficients": {"r11": -1.0, "r10": 1.0}},
-        ],
-    }
-    c3 = PolyhedralIoContract.from_dict(contract3)
-
-    fig = plot_guarantees(
-        contract=c3,
-        x_var=Var("t10"),
-        y_var=Var("soc11"),
-        var_values={
-            # Var("t10"):0,
-            Var("t11"): 2,
-            Var("d10"): 1,
-            Var("d11"): 0,
-            Var("e10"): 0,
-            Var("e11"): 0,
-            Var("r10"): 0,
-            Var("r11"): 0,
-            Var("soc10"): 6,
-        },
-        x_lims=(-2, 2),
-        y_lims=(-2, 2),
-    )
-
-    plt.show()
